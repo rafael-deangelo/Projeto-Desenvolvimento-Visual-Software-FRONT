cd2d187c00a31e781d9933789ca39cd7e66b2c31
ekuritza <elkuritza@gmail.com>
2022-11-26 14:42:05 -0300

    feat: add morador component with html and TS

--- src/app/components/pages/morador/cadastrar-morador/cadastrar-morador.component.ts
+++ src/app/components/pages/morador/cadastrar-morador/cadastrar-morador.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Morador } from 'src/app/models/Morador';
 
 @Component({
   selector: 'app-cadastrar-morador',
@@ -6,5 +9,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastrar-morador.component.css']
 })
 export class CadastrarMoradorComponent {
+  nome!: string;
+  cpf!: string;
+  apart!: string;
+  email!: string;
+  bloco!: string;
+  modeloVeiculo!: string;
+  cor!: string;
+  placa!: string;
+  moradorid!: number;
+  erro!: string;
 
+  //incializar construtor
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute) { } 
+
+  cadastrar(): void {
+    let morador : Morador = {
+      nome : this.nome,
+      cpf : this.cpf,
+      apartamento : this.apart,
+      email : this.email,
+      bloco: this.bloco,
+      modeloVeiculo : this.modeloVeiculo,
+      cor : this.cor,
+      placa : this.placa,
+    };
+    // CONFIGURAÇÃO DA REQUISIÇÃO
+    this.http.post<Morador>('https://localhost:5001/api/morador/cadastrar',
+      morador
+    )
+
+    // EXECUÇÃO DA REQUISIÇÃO
+    .subscribe({
+      // AQUI EXECUTAMOS ALGO QUANDO A REQUISIÇÃO FOR BEM SUCEDIDA
+      next: (morador) => {
+        //NAVIGATE LEVARA PARA A LISTA
+        this.router.navigate(["pages/morador/listar"]);
+      },
+      // AQUI EXECUTAMOS ALGO QUANDO A REQUISIÇÃO FOR MAL SUCEDIDA
+      error: (error) => {
+        if(error.status == 400){
+          this.erro = "Erro de validação!";
+        }else if(error.status == 0){
+          this.erro = "Está faltando iniciar a sua API!";
+        }
+      },
+    });
+   }
+
+  alterar(): void {
+    let morador : Morador = {
+      moradorId : this.moradorid,
+      nome : this.nome,
+      cpf : this.cpf,
+      apartamento : this.apart,
+      email : this.email,
+      bloco: this.bloco,
+      modeloVeiculo : this.modeloVeiculo,
+      cor : this.cor,
+      placa : this.placa,
+    };
+    // CONFIGURAÇÃO DA REQUISIÇÃO
+    this.http.patch<Morador>('https://localhost:5001/api/morador/alterar',
+      morador
+    )
+
+    // EXECUÇÃO DA REQUISIÇÃO
+    .subscribe({
+      // AQUI EXECUTAMOS ALGO QUANDO A REQUISIÇÃO FOR BEM SUCEDIDA
+      next: (morador) => {
+        this.router.navigate(["pages/morador/listar"]);
+      },
+      // AQUI EXECUTAMOS ALGO QUANDO A REQUISIÇÃO FOR MAL SUCEDIDA
+      error: (error) => {
+        if(error.status == 400){
+          this.erro = "Erro de validação!";
+        }else if(error.status == 0){
+          this.erro = "Está faltando iniciar a sua API!";
+        }
+      },
+    });
+  }
 }
---
+++ src/app/models/Morador.ts
@@ -0,0 +1,11 @@
+export interface Morador {
+    moradorId?: number;    
+    nome: string;
+    cpf: string;
+    apartamento: string;
+    email: string;
+    bloco: string;
+    modeloVeiculo: string;
+    cor: string;
+    placa: string;
+}
