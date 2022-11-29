c2de9aa0771a27d9f33e48e078f92fa6a1735dfe
rafael-deangelo <rafaelangelo15@hotmail.com>
2022-11-29 07:33:32 -0300

    feat: Primeiras alterações de Front tela principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +19,8 @@ import { CadastrarMoradorComponent } from './components/pages/morador/cadastrar-
 import { ListarMoradorComponent } from './components/pages/morador/listar-morador/listar-morador.component';
 import { CadastrarVagaComponent } from './components/pages/vagas/cadastrar-vaga/cadastrar-vaga.component';
 import { ListarVagasComponent } from './components/pages/vagas/listar-vagas/listar-vagas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +35,17 @@ import { ListarVagasComponent } from './components/pages/vagas/listar-vagas/list
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatFormFieldModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
